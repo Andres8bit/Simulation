a692b56ff436d30858c7ebacc6f38b14d7c17f2b
Andres Carrillo <andresc@cpp.edu>
2021-09-21 02:11:13 -0700

    Add files via upload

--- physics/src/Vec.cpp
+++ physics/src/Vec.cpp
@@ -18,7 +18,7 @@ double dot(const Vec& x, const Vec& y){
 }
 
 double magnitude(const Vec& x) {
-    return sqrt((x.x * x.x) + (x.y * x.y));
+    return sqrt(lenSqrd(x));
 }
 
 Vec operator/(const Vec& y, const double& x) {
@@ -33,4 +33,44 @@ double distance(const Vec& x, const Vec& y) {
     double yDel = (y.y - x.y) * (y.y - y.x);
 
     return sqrt(xDel + yDel);
+}
+
+Vec inverse(const Vec& x) {
+    return Vec(-x.x, -x.y);
+}
+
+Vec polar(const double& angle,const double& radius){
+    return Vec(radius * cos(angle), radius * sin(angle));
+}
+
+double angle(const Vec& x){
+    return  atan2(x.y, x.x);
+}
+
+Vec norm(const Vec& x) {
+    return x / magnitude(x);
+}
+
+Vec translate(const Vec& x, const double& tx, const double& ty) {
+    return Vec(x.x + tx, x.y + ty);
+}
+
+Vec rotate(const Vec& x, const float& rad) {
+    new Vec(x.x * cos(rad) - x.y * sin(rad), x.x * sin(rad) + x.y * cos(rad));
+}
+
+Vec shear(const Vec&x, const float& sx, const float& sy) {
+    return Vec(x.x + sx * x.y, x.y + sy * x.x);
+}
+
+double lenSqrd(const Vec& x){
+    return x.x *x.x  + x.y * x.y;
+}
+
+Vec perp(const Vec& x) {
+    return Vec(-x.y, x.x);
+}
+
+Vec scale(const Vec& x, const double& sx, const double& sy) {
+    return Vec(x.x * sx, x.y * sy);
 }
--- physics/src/Vec.h
+++ physics/src/Vec.h
@@ -1,14 +1,16 @@
 #pragma once
 #include<math.h>
+
+
 struct Vec {
 	double x, y;
     Vec(double x = 0, double y = 0){ this->x = x; this->y = y; }
     Vec(const Vec& val) { x = val.x; y = val.y; }
+   
     Vec& operator=(Vec const other) {
         if (this == &other)
             return *this;
-        this->x = other.x;
-        this->y = other.y;
+        copy(other);
         return *this;
     }
     
@@ -26,4 +28,30 @@ struct Vec {
         double magnitude(const Vec& x);
     friend
         double distance(const Vec& x, const Vec& y);
+    friend
+        Vec inverse(const Vec& x);
+    friend
+        Vec polar(const double& angle, const double& radius);
+    friend
+        double angle(const Vec& x);
+    friend
+        Vec norm(const Vec& x);
+    friend
+        double lenSqrd(const Vec& x);
+    friend
+        Vec perp(const Vec& x);
+    friend
+        Vec translate(const Vec& x, const double& tx, const double& ty);
+    friend
+        Vec rotate(const Vec& x, const float& rad);
+    friend
+        Vec shear(const Vec& x, const float& sx, const float& sy);
+    friend
+        Vec scale(const Vec& x, const double& sx, const double& sy);
+
+private:
+        void copy(const Vec& other){
+            this->x = other.x;
+            this->y = other.y;
+        }
 };
